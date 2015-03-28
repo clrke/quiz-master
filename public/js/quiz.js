192dadb499fdb644551c57c81f2db0226b712013
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-03-29 02:53:04 +0800

    Add question timer

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -6,7 +6,7 @@ angular.module('QuizApp', [])
 .controller('QuestionController', ['$http', '$interval',
 		function($http, $interval){
 	Question = this;
-	Question.value = "What are the needed ingredients for Buriza?";
+	Question.value = "Anong buong pangalan ng Buriza?";
 	Question.iteration = 0;
 
 	Question.type = function () {
@@ -14,9 +14,25 @@ angular.module('QuizApp', [])
 
 		// todo: stop iteration
 	}
-	Question.typer = $interval(Question.type, 50);
+	Question.typer = $interval(Question.type, 100);
+
+	Question.time = 30;
+	Question.timeMinus = function () {
+		Question.time--;
+
+		// todo: stop iteration
+	}
+	Question.startTimer = function () {
+		Question.time = 30;
+		if( ! Question.timer)
+			Question.timer = $interval(Question.timeMinus, 1000);
+	}
 
 	Question.getValue = function () {
 		return Question.value.substring(0, Question.iteration);
+	}
+
+	Question.getFormattedTime = function () {
+		return ("%2d", Question.time);
 	}
 }]);
