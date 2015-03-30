03e3a234bb76c477c9fbaf492c5332a2f68d4488
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-03-30 21:23:39 +0800

    Add node.js server

---
+++ index.js
@@ -0,0 +1,49 @@
+var http = require('http'),
+	fs = require('fs'),
+	url = require('url');
+
+http.createServer(function (request, response) {
+	var route = url.parse(request.url);
+
+	function setResponse (contentType, filename) {
+		fs.readFile(filename, function readFile (err, contents) {
+			if(err) {
+				response.writeHead(502, {'Content-Type': 'text/text'});
+				response.end('502 Bad gateway');
+			} else {
+				response.writeHead(200, {'Content-Type': contentType});
+				response.end(contents);
+			}
+		});
+	}
+
+	switch(route.path) {
+		case '/':
+			setResponse('text/html', 'index.html');
+			break;
+		case '/foundation.css':
+			setResponse('text/css', 'foundation/css/foundation.css');
+			break;
+		case '/main.css':
+			setResponse('text/css', 'main.css');
+			break;
+		case '/animate.css':
+			setResponse('text/css', 'animate.css');
+			break;
+		case '/angular.js':
+			setResponse('text/javascript', 'angular.js');
+			break;
+		case '/quiz.js':
+			setResponse('text/javascript', 'public/js/quiz.js');
+			break;
+		case '/description.json':
+			setResponse('text/json', 'description.json');
+			break;
+		default:
+			response.writeHead(404, {'Content-Type': 'text/text'});
+			response.end('404 page not found');
+	}
+}).listen(8000, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:8000/');
+
--- public/js/quiz.js
+++ public/js/quiz.js
@@ -6,37 +6,40 @@ angular.module('QuizApp', [])
 .controller('QuestionController', ['$http', '$interval',
 		function($http, $interval){
 	Question = this;
-	Question.value = "Anong buong pangalan ng Buriza?\n"+
-						"a. Shadow blade\n"+
-						"b. Lothar's Edge\n"+
-						"c. Buriza Mae Show\n"+
-						"d. Buriza de Kyanon";
-	Question.iteration = 0;
 
-	Question.type = function () {
-		Question.iteration++;
-	}
-	Question.typer = $interval(Question.type, 100, Question.value.length);
+	(function getQuestionDescription () {
+		$http.get('description.json').success(function (data) {
+			Question.value = data.questions.easy[0].question;
 
-	Question.time = 30;
-	Question.timeMinus = function () {
-		Question.time--;
-	}
-	Question.startTimer = function () {
-		if( ! Question.timer) {
+			Question.iteration = 0;
+
+			Question.type = function () {
+				Question.iteration++;
+			}
+			Question.typer = $interval(Question.type, 100,
+				Question.value.length);
+
 			Question.time = 30;
-			Question.timer = $interval(Question.timeMinus,
-				1000, Question.time);
-		}
-	}
+			Question.timeMinus = function () {
+				Question.time--;
+			}
+			Question.startTimer = function () {
+				if( ! Question.timer) {
+					Question.time = 30;
+					Question.timer = $interval(Question.timeMinus,
+						1000, Question.time);
+				}
+			}
 
-	Question.getValue = function () {
-		value = Question.value.substring(0, Question.iteration);
-		return value.split('\n');
+			Question.getValue = function () {
+				value = Question.value.substring(0, Question.iteration);
+				return value.split('\n');
 
-	}
+			}
 
-	Question.getFormattedTime = function () {
-		return (Question.time < 10? '0':'')+Question.time;
-	}
+			Question.getFormattedTime = function () {
+				return (Question.time < 10? '0':'')+Question.time;
+			}
+		});
+	})();
 }]);
